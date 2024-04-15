6fdbc8172f3b3206de7104788ed58b4e55ce7ad9
Ho Nyeong <dev.honing@gmail.com>
2024-04-15 21:52:24 +0900

    Test: 유틸 함수 사용 점검

--- Web/Express/server.js
+++ Web/Express/server.js
@@ -1,12 +1,25 @@
 // Express 서버
-const express = require('express');
+const express = require("express");
+const { Route } = require("./utils");
 const app = express();
 const PORT = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello, Express!!');
-})
+// '/' 경로에 대한 핸들러 등록
+Route(app, "get", "/", (req, res) => {
+  res.send("Hello, Express!!");
+});
+
+// '/test' 경로에 대한 핸들러 등록
+Route(app, "get", "/test", (req, res) => {
+  res.send("/test 점검");
+});
+
+// '/test2' 경로에 대한 핸들러 등록
+Route(app, "get", "/test2", (req, res) => {
+  res.send("/test2 점검");
+});
+
 
 app.listen(PORT, () => {
   console.log(`서버 ON: http://localhost:${PORT}`);
-})
+});
